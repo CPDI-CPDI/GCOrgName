b4a915c03efc724a7973d30848efb6392aa933bf
MominAKhan2 <58056584+MominAKhan2@users.noreply.github.com>
2025-05-05 14:42:00 -0400

    Correction

--- Tools/compare_manuals.py
+++ Tools/compare_manuals.py
@@ -138,8 +138,6 @@ def main():
             count_cols += 1
 
         df_out = pd.DataFrame(dict(name_columns_data))
-        df_out.replace('', pd.NA, inplace=True)
-        df_out = df_out.dropna(how='all') 
         df_out.to_csv('gc_org_info_report.csv', index=False)
         
     except KeyError as e:
